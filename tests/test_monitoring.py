c348de626902642f37ba2138fd6c6316d8aa23c4
thebrierfox <96512459+thebrierfox@users.noreply.github.com>
2025-02-05 09:00:58 -0600

    Update test_monitoring.py

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -1,1 +1,10 @@
+import unittest
+from monitoring.monitoring import monitor_endpoint
 
+class TestMonitoring(unittest.TestCase):
+    def test_monitor_endpoint(self):
+        status, info = monitor_endpoint("https://www.google.com")
+        self.assertTrue(status)
+
+if __name__ == '__main__':
+    unittest.main()
